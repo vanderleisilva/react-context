c6ea829e4e22087083b073262f513a32f12414e1
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2019-09-18 23:08:06 +0200

    update context structure

---
+++ src/context/todo/initialState.js
@@ -0,0 +1,21 @@
+export default {
+  todos: [
+    {
+      id: 1,
+      name: 'Item 1',
+      done: false,
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+      done: false,
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+      done: false,
+    },
+  ],
+  filter: '',
+  update: {},
+};
