131e920ad905fac178521518472c1a54d3d5248e
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2019-09-18 22:13:50 +0200

    add app context

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,28 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-export default createContext([0, () => {}])
+const AppContext = createContext([{}, () => {}]);
+
+const AppProvider = ({ children }) => {
+  const [state, setState] = useState({
+    toDos: [
+      {
+        id: 1,
+        name: "Item 1",
+        done: false
+      },
+      {
+        id: 2,
+        name: "Item 2",
+        done: false
+      },
+      {
+        id: 3,
+        name: "Item 3",
+        done: false
+      }
+    ]
+  });
+  return <AppContext.Provider value={[state, setState]} children={children} />;
+};
+
+export { AppContext, AppProvider };
