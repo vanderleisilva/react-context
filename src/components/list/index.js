5e69a9d01c881ea17320f1d35933e9d51327f1c1
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2019-09-30 13:57:56 +0200

    add remove todo button

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useStore } from 'store';
-import { udpateTodo } from 'actions';
+import { udpateTodo, removeTodo } from 'actions';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,6 +9,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default () => {
   const [{ todos, filter }, dispatch] = useStore();
@@ -19,22 +21,31 @@ export default () => {
         <TableRow>
           <TableCell children='Description' />
           <TableCell children='Done' />
+          <TableCell children='Remove' style={{ width: 64 }} />
         </TableRow>
       </TableHead>
       <TableBody>
-        {todos.filter(t => RegExp(filter, 'i').test(t.name)).map(todo => (
-          <TableRow key={todo.id}>
-            <TableCell children={todo.name} />
-            <TableCell>
-              <Checkbox
-                checked={todo.done}
-                onChange={() =>
-                  dispatch(udpateTodo({ ...todo, done: !todo.done }))
-                }
-              />
-            </TableCell>
-          </TableRow>
-        ))}
+        {todos
+          .filter(t => RegExp(filter, 'i').test(t.name))
+          .map(todo => (
+            <TableRow key={todo.id}>
+              <TableCell children={todo.name} />
+              <TableCell>
+                <Checkbox
+                  checked={todo.done}
+                  onChange={() =>
+                    dispatch(udpateTodo({ ...todo, done: !todo.done }))
+                  }
+                />
+              </TableCell>
+              <TableCell>
+                <IconButton
+                  children={<DeleteIcon />}
+                  onClick={() => dispatch(removeTodo(todo))}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
       </TableBody>
     </Table>
   );
